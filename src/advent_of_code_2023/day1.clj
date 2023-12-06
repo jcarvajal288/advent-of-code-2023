a08142559303b14dba6ac86ae1c41f36db47b297
Max Carvajal <john.m.carvajal.ctr@army.mil>
2023-12-06 15:56:07 -0600

    Day 1 Part 2 complete

--- src/advent_of_code_2023/day1.clj
+++ src/advent_of_code_2023/day1.clj
@@ -9,3 +9,49 @@
        (map #(str/join "" %))
        (map str-to-int)
        (reduce +)))
+
+(defn contains-number-word? [word]
+  (let [number-words ["one" "two" "three" "four" "five" "six" "seven" "eight" "nine"]]
+    (boolean (some true? (map #(str/includes? word %) number-words)))))
+
+(defn get-number [word]
+  (cond
+    (str/includes? word "one") \1
+    (str/includes? word "two") \2
+    (str/includes? word "three") \3
+    (str/includes? word "four") \4
+    (str/includes? word "five") \5
+    (str/includes? word "six") \6
+    (str/includes? word "seven") \7
+    (str/includes? word "eight") \8
+    :else \9))
+
+(defn find-first-number [line]
+  (let [iterate (fn [current rest]
+                  (cond
+                    (is-digit? (first rest)) (first rest)
+                    (contains-number-word? (str current (first rest)))
+                                     (get-number (str current (first rest)))
+                    :else (recur (str current (first rest)) (next rest))))]
+    (iterate "" line)))
+
+(defn find-last-number [line]
+  (let [iterate (fn [current rest]
+                  (cond
+                    (is-digit? (last rest)) (last rest)
+                    (contains-number-word? (str (last rest) current))
+                                     (get-number (str (last rest) current))
+                    :else (recur (str (last rest) current) (butlast rest))))]
+    (iterate "" line)))
+
+(defn parse-number-words [line]
+  [(find-first-number line) (find-last-number line)])
+
+(defn find-calibration-value-sum-2 [filename]
+  (->> (get-resource-file-by-line filename)
+       (map #(parse-number-words %))
+       (map #(filter is-digit? %))
+       (map #(vec [(first %) (last %)]))
+       (map #(str/join "" %))
+       (map str-to-int)
+       (reduce +)))
--- test/advent_of_code_2023/day1_test.clj
+++ test/advent_of_code_2023/day1_test.clj
@@ -2,10 +2,18 @@
   (:require [clojure.test :refer :all]
             [advent-of-code-2023.day1 :refer :all]))
 
-(deftest day1-example
+(deftest day1-part1-example
   (testing "Day 1 example"
     (is (= (find-calibration-value-sum "day1-example.txt") 142))))
 
-(deftest day1-full
+(deftest day1-part1-full
   (testing "Day 1 full"
-    (is (= (find-calibration-value-sum "day1-full.txt") 142))))
+    (is (= (find-calibration-value-sum "day1-full.txt") 54953))))
+
+(deftest day1-part2-example
+  (testing "Day 2 example"
+    (is (= (find-calibration-value-sum-2 "day1-example-2.txt") 281))))
+
+(deftest day1-part2-full
+  (testing "Day 2 full"
+    (is (= (find-calibration-value-sum-2 "day1-full.txt") 53868))))
